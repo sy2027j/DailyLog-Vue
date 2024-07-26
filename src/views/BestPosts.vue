8b23f740bff45ad6b8b95deeecca9dd136e7fc22
seoyoung <sy2027j@naver.com>
2024-07-26 15:30:41 +0900

    design : 메인화면 구성

--- src/views/BestPosts.vue
+++ src/views/BestPosts.vue
@@ -1,6 +1,49 @@
 <template>
-    <div class="postContent">
-        <div>asf
+    <div class="postContents">
+        <div class="post">
+            <div class="contentTop flex">
+                <img class="mr20" alt="contentsPreviewImg" src="../assets/dailylog-logo.jpg" width="100px">
+                <div class="w100 txt_left">
+                    <div class="font22">데일리로그 포스트 레이아웃 타이틀</div>
+                    <div>데일리로그 포스트 레이아웃 콘텐츠 데일리로그 포스트 레이아웃 콘텐츠 
+                        데일리로그 포스트 레이아웃 콘텐츠 데일리로그 포스트 레이아웃 콘텐츠
+                        데일리로그 포스트 레이아웃 콘텐츠 데일리로그 포스트 레이아웃 콘텐츠
+                    </div>
+                </div>                   
+            </div>
+            <div class="contentBottom txt_left">
+                ♡ 100  ☞ 20
+            </div>
+        </div>
+        <div class="post">
+            <div class="contentTop flex">
+                <img class="mr20" alt="contentsPreviewImg" src="../assets/dailylog-logo.jpg" width="100px">
+                <div class="w100 txt_left">
+                    <div class="font22">데일리로그 포스트 레이아웃 타이틀</div>
+                    <div>데일리로그 포스트 레이아웃 콘텐츠 데일리로그 포스트 레이아웃 콘텐츠 
+                        데일리로그 포스트 레이아웃 콘텐츠 데일리로그 포스트 레이아웃 콘텐츠
+                        데일리로그 포스트 레이아웃 콘텐츠 데일리로그 포스트 레이아웃 콘텐츠
+                    </div>
+                </div>                   
+            </div>
+            <div class="contentBottom txt_left">
+                ♡ 100  ☞ 20
+            </div>
+        </div>
+        <div class="post">
+            <div class="contentTop flex">
+                <img class="mr20" alt="contentsPreviewImg" src="../assets/dailylog-logo.jpg" width="100px">
+                <div class="w100 txt_left">
+                    <div class="font22">데일리로그 포스트 레이아웃 타이틀</div>
+                    <div>데일리로그 포스트 레이아웃 콘텐츠 데일리로그 포스트 레이아웃 콘텐츠 
+                        데일리로그 포스트 레이아웃 콘텐츠 데일리로그 포스트 레이아웃 콘텐츠
+                        데일리로그 포스트 레이아웃 콘텐츠 데일리로그 포스트 레이아웃 콘텐츠
+                    </div>
+                </div>                   
+            </div>
+            <div class="contentBottom txt_left">
+                ♡ 100  ☞ 20
+            </div>
         </div>
     </div>
 </template>
@@ -10,7 +53,43 @@ export default {
 }
 </script>
 <style>
-.postContent {
-    padding: 0 40px;
+.postContents {
+    padding: 0 7%;
+}
+.post {
+    padding: 10px 40px;
+    /* margin: 0px 10%; */
+    border-bottom: 2px solid #f1f3f5;
+    height: 143px;
+}
+.contentTop {
+    height: 100px;
+}
+.contentBottom {
+    height: 23px;
+}
+.flex {
+    display: flex;
+}
+.w100 {
+    width: 100%;
+}
+.pd10 {
+    padding: 10px;
+}
+.wpd10 {
+    padding: 10px 0px;
+}
+.hpd10 {
+    padding: 0px 10px;
+}
+.font22 {
+    font-size: 22px;
+}
+.mr20 {
+    margin-right: 20px;
+}
+.txt_left {
+    text-align: left;
 }
 </style>
