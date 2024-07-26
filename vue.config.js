63852dbdf636747e76632765842371d226697fb1
seoyoung <sy2027j@naver.com>
2024-07-26 13:09:34 +0900

    build : publicPath 추가

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: '../src/main/resources/static/vueStatic',
+	transpileDependencies: true,
+	outputDir: '../src/main/resources/static/vueStatic',
+	publicPath: '/vueStatic/',
 	devServer: {
 		proxy: 'http://localhost'
 	}
