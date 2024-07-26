c50db5685ad5905a158d70b6ce18ad3f59f2c95b
seoyoung <sy2027j@naver.com>
2024-07-26 17:52:53 +0900

    design : 로그인 화면 구성

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import BestPosts from "./views/BestPosts";
 import RecommendPosts from "./views/RecommendPosts";
+import Login from "./views/Login";
 
 const routes = [
-    {path:"/", component: BestPosts},
-    {path:"/about", component: RecommendPosts}
+    {path:"/login", component: Login},
+    {path:"/posts/bestPosts", component: BestPosts},
+    {path:"/posts/recommendPosts", component: RecommendPosts}
 ]
 
 const router = createRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,105 @@
+<template>
+    <div id="loginDiv">
+        <h2 class="pdb10">
+            <img alt="Vue logo" src="../assets/dailylog-logo-full.jpg" width="300px">
+        </h2>
+        <div id="loginFormDiv">
+            <div class="pdb10"><input class="loginInput" id="id" placeholder="아이디"></div>
+            <div class="pdb20"><input class="loginInput" id="password" placeholder="비밀번호"></div>
+            <div class="pdb10"><button class="loginBtn" id="login">로그인</button></div>
+        </div>
+        <div class="loginLinksDiv pdb20">
+            <a class="" href="/signin/find/password">비밀번호 찾기</a><a> | </a>
+            <a class="" href="/signup">회원가입</a><a> | </a>
+            <a class="" href="/signin/find/id">아이디(이메일) 찾기</a>
+        </div>
+        <div class="hr-text pdb20">간편 로그인</div>
+        <div class="">
+            <div class="">
+                <button class="socialLoginBtn pr10" type="button" id="kakaoLoginBtn" aria-label="카카오로 로그인">
+                    <img alt="kakao logo" src="../assets/socialLogin/btn_kakao.svg" width="30px">
+                </button>
+                <button class="socialLoginBtn pr10" type="button" id="googleLoginBtn" aria-label="구글로 로그인">
+                    <img alt="google logo" src="../assets/socialLogin/btn_google.svg" width="30px">
+                </button>
+                <button class="socialLoginBtn pr10" type="button" id="naverLoginBtn" aria-label="네이버로 로그인">
+                    <img alt="naver logo" src="../assets/socialLogin/btn_naver.svg" width="30px">
+                </button>
+                <button class="socialLoginBtn" type="button" id="appleLoginBtn" aria-label="애플로 로그인">
+                    <img alt="apple logo" src="../assets/socialLogin/btn_apple.svg" width="30px">
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'RecommendPosts',
+}
+</script>
+<style>
+#loginDiv {
+    width: 500px;
+    padding: 20px 20px;
+    margin: 0 auto;
+}
+.pdb10 {
+    padding-bottom: 10px;
+}
+.pdb20 {
+    padding-bottom: 20px;
+}
+.loginInput {
+    width: 400px;
+    height: 45px;
+    border: 0.0625rem solid rgb(206, 212, 218);
+    border-radius: 0.5rem;
+    padding: 0 15px;
+}
+.loginBtn {
+    width: 400px;
+    height: 45px;
+    border: 0.0625rem solid #0099e5;
+    border-radius: 0.5rem;
+    padding: 0 15px;
+    background-color: #0099e5;
+    color: white;
+    font-weight: 600;
+}
+.loginLinksDiv {
+    font-size: 12px;
+}
+a {
+    text-decoration: none !important;
+    color: black !important;
+}
+.hr-text {
+    display: flex;
+    flex-basis: 100%;
+    align-items: center;
+    color: rgba(0, 0, 0, 0.35);
+    font-size: 12px;
+    margin: 8px 0px;
+}
+.hr-text::before,
+.hr-text::after {
+    content: "";
+    flex-grow: 1;
+    background: rgba(0, 0, 0, 0.35);
+    height: 1px;
+    font-size: 0px;
+    line-height: 0px;
+    margin: 0px 16px;
+}
+.socialLoginBtn {
+    border: 0;
+    background-color: transparent;
+}
+h2 {
+    color: #0099e5 !important;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+.pr10 {
+    padding-right: 10px;
+}
+</style>
