7716a29daa661f518767949ba09f0cd7b3379e35
seoyoung <sy2027j@naver.com>
2024-10-28 23:00:50 +0900

    feat : OAuth2 로그인 처리 보완

--- src/views/UserRedirect.vue
+++ src/views/UserRedirect.vue
@@ -6,12 +6,21 @@
 <script>
   export default {
     mounted() {
-      const token = this.$store.state.token
-      console.log('token',token)
-      if (token) {
-        this.$store.commit('setToken', token);
-        window.alert('로그인하였습니다');
-        this.$router.push({path:'/'})
+      const urlParams = new URLSearchParams(window.location.search);
+      const userEmail = urlParams.get('userEmail');
+      if (userEmail) {
+        const loginForm = {
+            email: userEmail,
+        }
+        this.$axios.post('/api/auth/user', loginForm).then(res => {
+          if (res.status === 200) {
+              this.$store.commit('setToken', res.data.data.accessToken);
+              this.$store.commit('setUserInfo', res.data.data.userInfo);
+              this.$router.push({path:'/dailylog/posts/bestPosts'})
+          }
+        }).catch(() => {
+          window.alert('로그인에 실패하였습니다.')
+        })
       } else {
         window.alert('로그인에 실패하였습니다.')
         this.$router.push({path:'/login'})
