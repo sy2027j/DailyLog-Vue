ee6b75948001f356dd57d2b6f30d09995cacee11
seoyoung <sy2027j@naver.com>
2024-12-21 13:11:18 +0900

    feat : 내 정보 조회/수정 기능 추가

--- src/views/MyInfo.vue
+++ src/views/MyInfo.vue
@@ -2,31 +2,31 @@
     <div class="infoContents">
         <div class="flex betweenBox pdt10">
             <div> 
-                <img :src="previewUrl || $store.state.userInfo.profile" alt="프로필 사진" class="profile-image profile-b"/>
+                <img :src="previewUrl || userInfo.profile" alt="프로필 사진" class="profile-image profile-b"/>
                 <button class="bg_profile" @click="triggerFileInput">사진 변경</button>
                 <input type="file" ref="fileInput" @change="handleFileChange" accept="image/*" style="display: none;"/>
             </div>
             <div class="infoBox">
                 <div class="pdb10">
                     <span class="block userInfos">이메일</span>
-                    <input class="userInput" id="email" type="email" v-model="email" required>
+                    <input class="userInput" id="email" type="email" :value="userInfo.email" readonly>
                 </div>
                 <div class="pdb10">
                     <span class="block userInfos">닉네임</span>
-                    <input class="userInput" id="nickname" type="text" v-model="nickname" maxlength="20" required>
+                    <input class="userInput" id="nickname" type="text" v-model="nickname" required>
                 </div>
                 <div class="pdb10">
                     <span class="block userInfos">계정 연동 정보</span>
-                    <div  class="userDiv flex betweenBox" v-for="login in socialLogin" :key="login.Id">
+                    <div  class="userDiv flex betweenBox" v-for="socialAccount in userInfo.socialAccounts" :key="socialAccount.Id">
                         <div class="flex alignCenter">
-                            <img :alt="`${login.provider} logo`" :src="require(`../assets/socialLogin/btn_${login.provider}.svg`)" width="30px">
-                            <label>{{ getProviderName(login.provider) }}</label>
+                            <img :alt="`${socialAccount.provider} logo`" :src="require(`../assets/socialLogin/btn_${socialAccount.provider}.svg`)" width="30px">
+                            <label>{{ getProviderName(socialAccount.provider) }}</label>
                         </div>
-                        <button class="bg_cancle" @click="disconnectProvider(login.provider)">연결 해제</button>
+                        <button class="bg_cancle" @click="disconnectProvider(socialAccount.id)">연결 해제</button>
                     </div>
                 </div>
                 <div class="pdt10 userInfos userBtn">
-                    <button class="bg_save"  @click="save">저장</button>
+                    <button class="bg_save"  @click="saveUserInfo">저장</button>
                 </div>
             </div>
         </div>
@@ -37,10 +37,10 @@ export default {
     name: 'MyInfo',
     data() {
         return {
-            email: '',
             nickname: '',
             socialLogin: [],
             previewUrl: null,
+            userInfo: []
         };
     },
     methods: {
@@ -64,15 +64,63 @@ export default {
             };
             return providerNames[provider] || provider;
         },
+        disconnectProvider(accountId) {
+            alert(accountId)
+            this.$axios.delete(`/api/user/social`, { 
+                    params: { socialAccountId: accountId }
+                }).then(() => {
+                    this.$axios.get(`/api/user/social`, { 
+                    }).then(res => {
+                        this.userInfo.socialAccounts = res.data.list;
+                    }).catch((error) => {
+                        console.error("연동 해지 작성 실패:", error);
+                    });
+                }).catch((error) => {
+                    console.error("연동 해지 실패:", error);
+                });
+        },
         validateNickname() {
             const restrictedChars = /[<>#]/g;
             this.nickname = this.nickname.replace(restrictedChars, "");
         },
-        getUser() {
+        getUserInfo() {
             this.$axios.get(`/api/user`, [], {
             }).then(res => {
                 if (res.status === 200) {
-                    console.log('성공:');
+                    this.userInfo = res.data.data;
+                    this.nickname = this.userInfo.nickname;
+                    this.$store.commit('updateUserProfile', {
+                        profile: this.userInfo.profile,
+                        nickname: this.nickname
+                    });
+                }
+            }).catch((error) => {
+                console.error("불러오기 오류:", error);
+                this.loadingMessage = '불러오기 오류';
+            })
+        },
+        saveUserInfo() {
+
+            const userForm = new FormData();
+
+            const userRequest = {
+                nickname: this.nickname
+            };
+            userForm.append("userRequest", new Blob([JSON.stringify(userRequest)], { type: "application/json" }));
+
+            if (this.$refs.fileInput.files.length > 0) {
+                const file = this.$refs.fileInput.files[0];
+                userForm.append("profileImage", file);
+            }
+
+            this.$axios.post(`/api/user`, userForm, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }).then(res => {
+                if (res.status === 200) {
+                    alert("수정 성공");
+                    this.getUserInfo();
                 }
             }).catch((error) => {
                 console.error("불러오기 오류:", error);
@@ -81,7 +129,7 @@ export default {
         },
     },
     mounted() {
-        this.getUser()
+        this.getUserInfo();
     },
 }
 </script>
@@ -110,6 +158,13 @@ export default {
     border: 0.0625rem solid rgb(206, 212, 218);
     border-radius: 0.5rem;
     padding: 0 15px;
+}
+input.userInput:read-only {
+    background-color: rgb(235 236 238);
+}
+input.userInput:read-only:focus {
+    outline: none;
+    border-color: rgb(206, 212, 218);
 }
 .userDiv {
     width: 85%;
