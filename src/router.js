5381854654eb46cd4954b5ca11125e86e88e1e5e
seoyoung <sy2027j@naver.com>
2024-12-24 14:36:29 +0900

    feat : 라우터 가드 추가
    - 'meta: { requiresAuth: true }'를 이용하여 인증이 필요한 페이지 구분
    - 'beforeEach' 라우터 가드를 통해 인증되지 않은 사용자는 로그인 페이지로 리다이렉트

--- src/router.js
+++ src/router.js
@@ -9,23 +9,33 @@ import WritePost from "./views/WritePost.vue";
 import PostDetail from './views/PostDetail.vue';
 import UserHome from './views/UserHome.vue';
 import MyInfo from './views/MyInfo.vue';
+import store from './store';
 
 const routes = [
     {path:"/dailylog/login", component: Login},
     {path:"/dailylog/join", component: Join},
     {path:"/dailylog/posts/bestPosts", component: BestPosts},
-    {path:"/dailylog/posts/subscribePosts", component: SubscribePosts},
+    {path:"/dailylog/posts/subscribePosts", component: SubscribePosts, meta: { requiresAuth: true }},
     {path:"/dailylog/oauth2/redirect", component: Redirect},
-    {path:"/dailylog/mypage/newPost", component: WritePost},
+    {path:"/dailylog/mypage/newPost", component: WritePost, meta: { requiresAuth: true }},
     {path:"/dailylog/posts/:postId", component: PostDetail},
-    {path:"/dailylog/mypage/home/:email", component: UserHome},
-    {path:"/dailylog/user/home/:email", component: UserHome},
-    {path:"/dailylog/mypage/myInfo", component: MyInfo},
+    {path:"/dailylog/mypage/home/:email", component: UserHome, meta: { requiresAuth: true }},
+    {path:"/dailylog/user/home/:email", component: UserHome, meta: { requiresAuth: true }},
+    {path:"/dailylog/mypage/myInfo", component: MyInfo, meta: { requiresAuth: true }},
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+router.beforeEach((to, from, next) => {
+    const token = store.state.token
+    if (to.meta.requiresAuth && !token) {
+        next({ path: '/dailylog/login' });
+    } else {
+        next();
+    }
 });
 
 export {router}
